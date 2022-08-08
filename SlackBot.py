404589b621d6bda1e2ca5201cc269e6518be1109
tartem26 <tartem26@gmail.com>
2022-08-07 23:39:17 -0700

    app_mention - Hello World

--- SlackBot.py
+++ SlackBot.py
@@ -1,2 +1,28 @@
+import logging
+import os
+from dotenv import load_dotenv
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# Logging levels
+logging.basicConfig(level=logging.INFO)
+
+load_dotenv()
+
+# read the environment variables
+SLACK_BOT_TOKEN = os.environ["bot_user_oauth_token"]
+SLACK_APP_TOKEN = os.environ["socket_mode_token"]
+
+app = App(token=SLACK_BOT_TOKEN)
+
+@app.event("app_mention")
+def mention_handler(body, context, payload, options, say, event):
+    say("Hello World!")
+
+@app.event("message")
+def message_handler(body, context, payload, options, say, event):
+    pass
+
 if __name__ == "__main__":
-    print("Hello World")
+    handler = SocketModeHandler(app, SLACK_APP_TOKEN)
+    handler.start()
