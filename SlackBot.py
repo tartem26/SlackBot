0165d43c9e6b2f690695a142cec910aa277e620b
tartem26 <tartem26@gmail.com>
2022-08-08 02:37:47 -0700

    Command logic

--- SlackBot.py
+++ SlackBot.py
@@ -16,12 +16,33 @@ SLACK_APP_TOKEN = os.environ["socket_mode_token"]
 app = App(token=SLACK_BOT_TOKEN)
 
 @app.event("app_mention")
-def mention_handler(body, context, payload, options, say, event):
-    say("Hello World!")
+def mention_handler(body, context, payload, options, say, event, logger):
+    request = payload['blocks'][0]['elements'][0]['elements'][1]['text']
+    # logger.info(request)
+    res = request.lstrip().split(' ', 1)
+    logger.info(res)
+    cmd = res[0].strip().lower()
+    if len(res) > 1:
+        params = res[1].lstrip()
+    else:
+        params = ""
+    logger.info(cmd)
+    logger.info('------')
 
-@app.event("message")
-def message_handler(body, context, payload, options, say, event):
-    pass
+    if cmd == "help":
+        command1(request, cmd, params, say, logger)
+    elif cmd == "sleep":
+        command2(request, cmd, params, say, logger)
+    else:
+        say("It was a really fascinating request: " + request)
+
+
+def command1(request, cmd, params, say, logger):
+    say("command1: " + params)
+
+def command2(request, cmd, params, say, logger):
+    say("command2: " + params)
+
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
